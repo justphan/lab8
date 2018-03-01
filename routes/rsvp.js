34dd61cb5fbc143e8b7e3efdb4c7880b6d618708
Justin Phan <juphan@ucsd.edu>
2018-03-01 01:41:35 -0800

    thirdpartyAPI lab

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -2,6 +2,15 @@ var data = {
   rsvp: ['ixd@ucsd.edu']
 };
 
+exports.addRSVP = function(req, res){
+	var rsvpEmail = req.body.rsvpEmail;
+
+	console.log(rsvpEmail);
+
+	data.rsvp.push(rsvpEmail);
+
+	res.send(rsvpEmail);
+}
 /*
  * GET tasks page.
  */
